cfe46cf039d690ead7f067608026e1991aa1cda0
root@ali2 <tdouguo@163.com>
2021-09-30 14:49:11 +0800

    first commit

---
+++ js/tj.js
@@ -0,0 +1,9 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c2b28ac6e4bbe9b6c3ccc7b29ef1b28d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+//GOOGLE Analytics
